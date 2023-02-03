e428024713ac82394a6ff1222016102f7bfe27ef
anubhav <anubhavshivahre@gmail.com>
2023-02-03 16:13:29 +0530

    added error classes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
+const db = require('./models/index');
 
 const createAndStartServer = () => {
     const app = express();
@@ -13,8 +14,10 @@ const createAndStartServer = () => {
 
     app.listen(PORT,() => {
         console.log(`Server started on PORT: ${PORT}`);
-
-
+        if (process.env.DB_SYNC) {
+            db.sequelize.sync({ alter: true });
+        }
+        
     });
 }
 
